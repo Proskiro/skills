7bb4d2db57b7a8d5fa539c7db3ad7830d93d711b
Esa Ikram <eikram@costar.co.uk>
2025-10-30 15:12:54 +0000

    adding more base files

--- my_scraper/items.py
+++ my_scraper/items.py
@@ -4,43 +4,29 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
 
 
 class OccupationItem(scrapy.Item):
-    uri = scrapy.Field()
     preferred_label = scrapy.Field()
-    alt_labels = scrapy.Field()
+    alt_label = scrapy.Field()
     description = scrapy.Field()
-    isco_group = scrapy.Field()
+    isco_code = scrapy.Field()
+    uri = scrapy.Field()
     concept_type = scrapy.Field()
-    status = scrapy.Field()
-    modified_date = scrapy.Field()
-    broader_uris = scrapy.Field()
-    narrower_uris = scrapy.Field()
-    essential_skill_uris = scrapy.Field()
-    optional_skill_uris = scrapy.Field()
-    source_language = scrapy.Field()
-    version = scrapy.Field()
-    source_url = scrapy.Field()
-    scraped_at = scrapy.Field()
+    broader = scrapy.Field()
+    narrower = scrapy.Field()
+    essential_skills = scrapy.Field()
+    optional_skills = scrapy.Field()
+    is_leaf_node = scrapy.Field()
 
 
 class SkillItem(scrapy.Item):
     uri = scrapy.Field()
-    preferred_label = scrapy.Field()
-    alt_labels = scrapy.Field()
+    skillType = scrapy.Field()
+    preferredLabel = scrapy.Field()
     description = scrapy.Field()
-    concept_type = scrapy.Field()
-    reuse_level = scrapy.Field()
-    skill_type = scrapy.Field()
-    status = scrapy.Field()
-    broader_uris = scrapy.Field()
-    narrower_uris = scrapy.Field()
-    modified_date = scrapy.Field()
-    source_url = scrapy.Field()
 
 
-class OccupationSkillItem(scrapy.Item):
-    occupation_uri = scrapy.Field()
-    skill_uri = scrapy.Field()
-    skill_type = scrapy.Field()  # "essential" or "optional"
+class SkillLoader(ItemLoader):
+    default_item_class = SkillItem
