fda4ec6031950b701e55a24eb8614018d2c634f0
Esa Ikram <eikram@costar.co.uk>
2025-11-05 10:12:49 +0000

    initial script and infrastructure works

--- my_scraper/items.py
+++ my_scraper/items.py
@@ -4,7 +4,6 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader import ItemLoader
 
 
 class OccupationItem(scrapy.Item):
@@ -13,20 +12,17 @@ class OccupationItem(scrapy.Item):
     description = scrapy.Field()
     isco_code = scrapy.Field()
     uri = scrapy.Field()
-    concept_type = scrapy.Field()
-    broader = scrapy.Field()
-    narrower = scrapy.Field()
-    essential_skills = scrapy.Field()
-    optional_skills = scrapy.Field()
+    broader_concept = scrapy.Field()
+    broader_isco_group = scrapy.Field()
+    narrower_concept = scrapy.Field()
+    narrower_occupation = scrapy.Field()
+    essential_skills = scrapy.Field()  # array of skills items
+    optional_skills = scrapy.Field()  # array of skills items
     is_leaf_node = scrapy.Field()
 
 
 class SkillItem(scrapy.Item):
     uri = scrapy.Field()
-    skillType = scrapy.Field()
-    preferredLabel = scrapy.Field()
+    skill_type = scrapy.Field()
+    preferred_label = scrapy.Field()
     description = scrapy.Field()
-
-
-class SkillLoader(ItemLoader):
-    default_item_class = SkillItem
