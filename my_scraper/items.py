00c71a082659187d4bafa3fafa017f48f0211892
Esa Ikram <eikram@costar.co.uk>
2025-11-06 16:39:12 +0000

    latest changes

--- my_scraper/items.py
+++ my_scraper/items.py
@@ -12,13 +12,19 @@ class OccupationItem(scrapy.Item):
     description = scrapy.Field()
     isco_code = scrapy.Field()
     uri = scrapy.Field()
-    broader_concept = scrapy.Field()
-    broader_isco_group = scrapy.Field()
+    class_name = scrapy.Field()  # used for link to api
+    broader_isco_group_uri = scrapy.Field()
     narrower_concept = scrapy.Field()
     narrower_occupation = scrapy.Field()
     essential_skills = scrapy.Field()  # array of skills items
     optional_skills = scrapy.Field()  # array of skills items
     is_leaf_node = scrapy.Field()
+
+
+class OccupationHierarchyItem(scrapy.Item):
+    parent_uri = scrapy.Field()
+    child_uri = scrapy.Field()
+    relation_type = scrapy.Field()
 
 
 class SkillItem(scrapy.Item):
